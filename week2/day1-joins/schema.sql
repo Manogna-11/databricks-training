2181d3a0208420e6a1fafdcbcff129ecb169e78d
Manogna-11 <manognabhoganaduni@gmail.com>
2026-05-17 20:53:58 +0530

    Update schema.sql

--- week2/day1-joins/schema.sql
+++ week2/day1-joins/schema.sql
@@ -1,23 +1,49 @@
-CREATE TABLE departments (
-dept_id INT PRIMARY KEY,
-dept_name VARCHAR(50)
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    manager_id INT,
+    dept_id INT
 );
 
-INSERT INTO departments (dept_id, dept_name) VALUES
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
+
+CREATE TABLE projects (
+    project_id INT PRIMARY KEY,
+    project_name VARCHAR(50),
+    emp_id INT
+);
+
+CREATE TABLE salary (
+    emp_id INT,
+    salary INT
+);
+
+CREATE TABLE contacts (
+    emp_id INT,
+    phone VARCHAR(15)
+);
+
+INSERT INTO departments VALUES
 (1, 'HR'),
 (2, 'IT'),
 (3, 'Finance'),
 (4, 'Marketing'),
 (5, 'Sales');
 
-Table: projects
-CREATE TABLE projects (
-project_id INT PRIMARY KEY,
-project_name VARCHAR(50),
-emp_id INT
-);
+INSERT INTO employees VALUES
+(1, 'Karthik', NULL, 1),
+(2, 'Ajay', 1, 1),
+(3, 'Vijay', 1, 2),
+(4, 'Vinay', 2, 2),
+(5, 'Meena', 3, 3),
+(6, 'Veer', NULL, 4),
+(7, 'Keerthi', 4, 5),
+(8, 'Priya', 4, 5);
 
-INSERT INTO projects (project_id, project_name, emp_id) VALUES
+INSERT INTO projects VALUES
 (1, 'Project A', 1),
 (2, 'Project B', 2),
 (3, 'Project C', 3),
