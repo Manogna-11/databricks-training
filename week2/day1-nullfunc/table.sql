4b24bd93bad9f03a735d59a7ddfa998d567e08ae
Manogna-11 <manognabhoganaduni@gmail.com>
2026-05-17 21:47:16 +0530

    Update table.sql

--- week2/day1-nullfunc/table.sql
+++ week2/day1-nullfunc/table.sql
@@ -1,1 +1,21 @@
-
+CREATE TABLE Employees (
+    emp_id INT,
+    name VARCHAR(50),
+    salary INT,
+    bonus INT,
+    manager_id INT
+);
+CREATE TABLE Orders (
+    order_id INT,
+    customer_name VARCHAR(50),
+    amount INT,
+    discount INT,
+    coupon_code VARCHAR(20)
+);
+CREATE TABLE Products (
+    product_id INT,
+    product_name VARCHAR(50),
+    price INT,
+    category VARCHAR(50),
+    stock INT
+);
